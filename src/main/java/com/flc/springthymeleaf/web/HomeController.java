b7334bd1d631c5b0308c9f8d1405da70882eb78f
Fernando Correa <paracorrea@gmail.com>
2023-11-06 06:43:07 -0300

    starting with spring security

--- src/main/java/com/flc/springthymeleaf/web/HomeController.java
+++ src/main/java/com/flc/springthymeleaf/web/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/ceasa")
+	@GetMapping("/")
 	public String pageStart(Model model) {
 		
 		
