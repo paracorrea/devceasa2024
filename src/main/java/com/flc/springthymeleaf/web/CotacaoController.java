290921e80060ed61efc7c0921236c7dc1340c077
paracorrea <paracorrea@gmail.com>
2024-02-07 14:03:51 -0300

    Cotacao cadastro ajusted search for codigo

--- src/main/java/com/flc/springthymeleaf/web/CotacaoController.java
+++ src/main/java/com/flc/springthymeleaf/web/CotacaoController.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
