c9c12f8e0bd0f0fc36e98702ad9281bccfba801f
paracorrea <paracorrea@gmail.com>
2024-06-21 11:05:32 -0300

    Feira repository reordenada

--- src/main/java/com/flc/springthymeleaf/repository/FeiraRepository.java
+++ src/main/java/com/flc/springthymeleaf/repository/FeiraRepository.java
@@ -18,19 +18,21 @@ public interface FeiraRepository extends JpaRepository<Feira, Long>, PagingAndSo
     Long findMaxNumero();
 
     default Feira findByDataFeiraAndStatusFeira(LocalDate data, String statusString) {
-        // Converter a string para o enum StatusFeira
         StatusFeira status = StatusFeira.valueOf(statusString);
         return findByDataFeiraAndStatusFeira(data, status);
     }
 
     Feira findByDataFeiraAndStatusFeira(LocalDate data, StatusFeira status);
 
-    boolean existsByDataFeira(LocalDate dataFeira); // Método existsByDataFeira do Spring Data JPA
-    // ... outros métodos ...
+    boolean existsByDataFeira(LocalDate dataFeira);
 
     Feira findByDataFeira(LocalDate dataFeira);
 
     Feira findByNumero(Long numero);
 
+    @Query("SELECT f FROM Feira f WHERE f.dataFeira BETWEEN :dataInicio AND :dataFim ORDER BY f.dataFeira DESC")
     Page<Feira> findByDataFeiraBetween(LocalDate dataInicio, LocalDate dataFim, Pageable pageable);
+
+    @Query("SELECT f FROM Feira f ORDER BY f.dataFeira DESC")
+    Page<Feira> findAll(Pageable pageable);
 }
